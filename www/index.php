dfd89e95e9cd91e9d142a053042349b91fc30470
ZahidHussain.Sheikh <ZahidHussain.Sheikh@thomsonreuters.com>
2019-12-01 18:02:37 +0530

    adding code files

---
+++ www/index.php
@@ -0,0 +1,6 @@
+<div class="elem-group">
+    <label for="captcha">Please Enter the Captcha Text</label>
+    <img src="captcha.php" alt="CAPTCHA" class="captcha-image"><i class="fas fa-redo refresh-captcha"></i>
+    <br>
+    <input type="text" id="captcha" name="captcha_challenge" pattern="[A-Z]{6}">
+</div>
